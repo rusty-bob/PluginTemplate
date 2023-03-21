6d3aa44ad2829ba181589599a4862c94a5483c4d
instant.sc <code@insc.cc>
2023-03-21 19:22:52 +0100

    Initial commit

---
+++ templates/exApiPlugin/Plugin_NameSettings.cs
@@ -0,0 +1,15 @@
+﻿using ExileCore.Shared.Interfaces;
+using ExileCore.Shared.Nodes;
+
+namespace Plugin_Name;
+
+public class Plugin_NameSettings : ISettings
+{
+    //Mandatory setting to allow enabling/disabling your plugin
+    public ToggleNode Enable { get; set; } = new ToggleNode(false);
+
+    //Put all your settings here if you can.
+    //There's a bunch of ready-made setting nodes,
+    //nested menu support and even custom callbacks are supported.
+    //If you want to override DrawSettings instead, you better have a very good reason.
+}
